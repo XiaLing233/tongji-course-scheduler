d75ae750713a007c42f6bcab9f1a789dbaf1b550
NatsuIce <wangxilin@tongji.edu.cn>
2026-06-28 18:15:53 +0800

    fix: API version, rm outdated localStorage

--- xkFrontendts/src/main.ts
+++ xkFrontendts/src/main.ts
@@ -1,5 +1,11 @@
 import './assets/base.css'
 
+const APP_VERSION = '2.0'
+if (localStorage.getItem('app_version') !== APP_VERSION) {
+    localStorage.clear()
+    localStorage.setItem('app_version', APP_VERSION)
+}
+
 import { createApp } from 'vue'
 import store from './store'
 import Antd from 'ant-design-vue';
